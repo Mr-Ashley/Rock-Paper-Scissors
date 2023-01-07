aaf9a38eed1961819bf23bb8db16ccc826645c4f
Ashley Abrahams <ashley.c.abrahams@gmail.com>
2023-01-07 20:14:59 +0200

    Add function to play a round

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     return choices[randomChoice];
   }
+
+  function playRound(playerSelection,computerSelection){
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+  }else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "You win! Rock beats Scissors";
+    }else {
+      return "You lose! Paper beats Rock";
+    }
+  }else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You win! Paper beats Rock";
+    }else {
+      return "You lose! Scissors beats Paper";
+    }
+  }else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      return "You win! Scissors beats Paper";
+    }else {
+      return "You lose! Rock beats Scissors";
+    }
+  }else {
+    return "Invalid input. Please enter Rock, Paper, or Scissors.";
+  }
+  }
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
